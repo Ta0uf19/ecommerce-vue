32ebd35a72e8b85ee7eefcb2bd424d4ffc9bd207
Ta0uf19 <taoufik.tribki@gmail.com>
2020-02-02 03:15:40 +0100

    project installation

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="card">
+            <div class="card-header">Login</div>
+            <div class="card-body">
+              <form action="" method="">
+                <div class="form-group row">
+                  <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
+                  <div class="col-md-6">
+                    <input type="text" id="email_address" class="form-control" name="email-address" required autofocus>
+                  </div>
+                </div>
+
+                <div class="form-group row">
+                  <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+                  <div class="col-md-6">
+                    <input type="password" id="password" class="form-control" name="password" required>
+                  </div>
+                </div>
+
+                <div class="form-group row">
+                  <div class="col-md-6 offset-md-4">
+                    <div class="checkbox">
+                      <label>
+                        <input type="checkbox" name="remember"> Remember Me
+                      </label>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-md-6 offset-md-4">
+                  <button type="submit" class="btn btn-primary">
+                    Login
+                  </button>
+                  <a href="#" class="btn btn-link">
+                    Forgot Your Password?
+                  </a>
+                </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import './assets/styles/custom.scss'
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+Vue.use(VueAxios, axios);
+Vue.axios.defaults.baseURL = 'https://127.0.0.1:8000/api';
+
+
+import router from './router/routes'
+Vue.router = router;
+
+Vue.use(require('@websanova/vue-auth'), {
+  auth: require('@websanova/vue-auth/drivers/auth/bearer.js'),
+  http: require('@websanova/vue-auth/drivers/http/axios.1.x.js'),
+  router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js')
+})
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
